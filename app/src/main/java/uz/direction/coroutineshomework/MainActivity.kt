5ddfe5694ce87ecef9ce38559dd7aea035d9f450
Aidein <sultanovazamat985@gmail.com>
2022-06-09 17:47:00 +0500

    Coroutines are implemented

--- app/src/main/java/uz/direction/coroutineshomework/MainActivity.kt
+++ app/src/main/java/uz/direction/coroutineshomework/MainActivity.kt
@@ -1,11 +1,22 @@
 package uz.direction.coroutineshomework
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var job: Job
     private lateinit var memesPresenter: MemesPresenter
+
+    val myCoroutineScope =
+        CoroutineScope(
+            Dispatchers.Main +
+                    CoroutineName("MainCoroutine") +
+                    CoroutineExceptionHandler { _, exception ->
+                        Log.e("EX", "Exception \"$exception\" is caught")
+                    })
 
     private val diContainer = DiContainer
 
@@ -14,15 +25,18 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as MemeView
         setContentView(view)
 
-        memesPresenter = MemesPresenter(diContainer.memesService, this)
-        view.presenter = memesPresenter
-        memesPresenter.attachView(view)
-        memesPresenter.onInitComplete()
+        job = myCoroutineScope.launch {
+            memesPresenter = MemesPresenter(diContainer.memesService, applicationContext)
+            view.presenter = memesPresenter
+            memesPresenter.attachView(view)
+            memesPresenter.onInitComplete()
+        }
     }
 
     override fun onStop() {
         if (isFinishing) {
             memesPresenter.detachView()
+            job.cancel()
         }
         super.onStop()
     }
--- app/src/main/java/uz/direction/coroutineshomework/MemeView.kt
+++ app/src/main/java/uz/direction/coroutineshomework/MemeView.kt
@@ -1,13 +1,16 @@
 package uz.direction.coroutineshomework
 
 import android.content.Context
+import android.os.SystemClock
 import android.util.AttributeSet
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target
+import kotlinx.coroutines.*
 
 class MemeView @JvmOverloads constructor(
     context: Context,
@@ -17,16 +20,26 @@ class MemeView @JvmOverloads constructor(
 
     var presenter: MemesPresenter? = null
 
+    private val myCoroutineScope =
+        CoroutineScope(
+            Dispatchers.IO +
+                    SupervisorJob() +
+                    CoroutineExceptionHandler { _, exception ->
+                        Log.e("EX", "Exception \"$exception\" is caught")
+                    })
+
     override fun onFinishInflate() {
         super.onFinishInflate()
         findViewById<Button>(R.id.button).setOnClickListener {
-            presenter?.onInitComplete()
+            myCoroutineScope.launch { presenter?.onInitComplete() }
         }
     }
 
     override fun populate(meme: Meme) {
         findViewById<TextView>(R.id.caption).text = meme.caption
+        Log.d("TAG", "Caption = ${meme.caption}")
         val img = findViewById<ImageView>(R.id.img_meme)
+        Log.d("TAG", "Image link = ${meme.image}")
         Glide.with(context).load(meme.image).into(img)
     }
 }
